33c86e7cf1bd729a64510be198446d43d2ea4d54
userltx <151753599+userltx@users.noreply.github.com>
2024-08-15 01:08:56 +0400

    12

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req,res) => {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 app.listen(8000);
