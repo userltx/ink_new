a64fe60563323f92f2e1f38e73c78d8b280867f1
userltx <151753599+userltx@users.noreply.github.com>
2024-08-20 00:56:16 +0400

    32

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const options = {
+    key: fs.readFileSync('./cert/certificate.key'),
+    cert: fs.readFile('./cert/certificate.crt')
+}
 const app = express();
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
-app.listen(8000);
+http.createServer(app).listen(8000);
+https.createServer(options, app).listen(443);
