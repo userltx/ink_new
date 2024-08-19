8c37af86befedd70b68480cd989047e8becb074f
userltx <151753599+userltx@users.noreply.github.com>
2024-08-20 02:26:22 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fs = require('fs');
 const options = {
     key: fs.readFileSync('./cert/certificate.key'),
-    cert: fs.readFile('./cert/certificate.crt')
+    cert: fs.readFileSync('./cert/certificate.crt')
 }
 const app = express();
 app.get('/', (req,res) => {
